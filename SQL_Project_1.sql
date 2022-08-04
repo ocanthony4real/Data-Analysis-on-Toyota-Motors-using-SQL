580e4a6f713b7146e04968d9098a7d3cd4449ab1
Anthony <ocanthony4real@gmail.com>
2022-08-04 15:13:26 +0100

    initial commit

--- SQL_Project_1.sql
+++ SQL_Project_1.sql
@@ -1,2 +1,25 @@
-USE DATABASE toyotamotors;
-select * from balance_sheet_table 
+USE toyotamotors;
+SELECT * FROM cashflow;
+    
+DESC cashflow;
+
+ALTER TABLE cashflow ADD PRIMARY KEY (Date);
+# changes Date column to primary key
+ 
+ALTER TABLE cashflow MODIFY Date date;
+# change the type of Date column from datetime to date
+
+SELECT * FROM balance_sheet;
+DESC balance_sheet;
+
+ALTER TABLE balance_sheet ADD PRIMARY KEY (Date);
+ALTER TABLE balance_sheet MODIFY Date date;
+
+SELECT * FROM financials;
+DESC financials;
+
+ALTER TABLE financials ADD PRIMARY KEY (Date);
+ALTER TABLE financials MODIFY Date date;
+
+SELECT c.Date, abs(FORMAT(c.Capital_Expenditures / f.Total_Revenue, 2)) FROM cashflow c INNER JOIN financials f on c.Date = f.Date
+# evaluates the CAPEX to revenue ratio of the company. From the result, there's been a decline in the ratio over the years. However, the ratios are pretty fair for an automobile company. 
